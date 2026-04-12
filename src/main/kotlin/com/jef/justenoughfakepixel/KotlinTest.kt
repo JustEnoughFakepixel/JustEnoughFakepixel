25247a00999e6bbae2de34b252f0449f4f442a2f
protocol-8 <106147194+protocol-8@users.noreply.github.com>
2026-04-12 23:17:26 +0530

    kotlin init wooohooo

---
+++ src/main/kotlin/com/jef/justenoughfakepixel/KotlinTest.kt
@@ -0,0 +1,12 @@
+package com.jef.justenoughfakepixel
+
+class KotlinTest {
+
+    companion object {
+        @JvmStatic
+        fun testKotlin() {
+            print("Kotlin lowk works twin it aint hard")
+        }
+    }
+
+}
