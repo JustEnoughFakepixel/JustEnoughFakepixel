a3595b8960cdaf51b487e98823b94aa6ff63b7af
Kanishka Prakash <106147194+protocol-8@users.noreply.github.com>
2026-04-19 20:21:03 +0530

    who even uses this bro :sob:

--- src/main/java/com/jef/justenoughfakepixel/JefMod.java
+++ src/main/java/com/jef/justenoughfakepixel/JefMod.java
@@ -40,8 +40,6 @@ public class JefMod {
         JefConfig.init();
         JefRepo.init();
         logger = Logger.getLogger("[JEF] ");
-        SessionChanger.getInstance().setUserOffline("Diyansh");
-        logger.info("USING SESSION CHANGER: PLEASE REMOVE FROM JefMod.java and Delete SessionChanger.java for release");
         WaypointStorage.getInstance().initFile(JefConfig.configDirectory);
         InventoryButtonStorage.getInstance().initFile(JefConfig.configDirectory);
         DianaStats.getInstance().initFile(JefConfig.configDirectory);
